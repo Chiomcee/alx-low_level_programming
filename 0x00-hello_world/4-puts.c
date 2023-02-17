5cc6c367ec3aa44b2a54d800b5530bb34b749340
Chioma Igwe <cigwe072013@gmail.com>
2023-02-16 17:11:48 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints a line with put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
