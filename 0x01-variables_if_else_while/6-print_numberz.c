669ce44162059a6e7bb6aa23e0cd2b9dbb32a551
Chioma Igwe <cigwe072013@gmail.com>
2023-02-17 20:23:46 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit of base 10 from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
