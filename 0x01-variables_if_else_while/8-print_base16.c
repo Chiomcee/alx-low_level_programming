68006fc8f370b9b280840a99f389470bb196649e
Chioma Igwe <cigwe072013@gmail.com>
2023-02-17 20:53:42 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Prints all numbers of base 6 in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
