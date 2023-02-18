617d7bc1d48ad0836f5fd27dc24c0257459620c7
Chioma Igwe <cigwe072013@gmail.com>
2023-02-17 19:37:24 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all alphabets in lower case except q and e
+ *
+ * Return: Aways 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
+		       
+
