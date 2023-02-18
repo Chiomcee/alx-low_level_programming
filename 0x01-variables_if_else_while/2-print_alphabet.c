46313d07a00ad5ffdf276fbe8290856e6173161f
Chioma Igwe <cigwe072013@gmail.com>
2023-02-17 18:42:04 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar('i');
+	putchar('\n');
+	return (0);
+}
+
