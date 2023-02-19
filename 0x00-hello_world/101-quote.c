6939288e03082f4e5535889611418382082cb398
Chioma Igwe <cigwe072013@gmail.com>
2023-02-18 16:04:03 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line to the standard error
- * return 1 (Success)
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *  followed by a new line, to the standard error
+ *  Return: Always 1
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
