9ffc16bcc9a12a18a789ed5db68b7ea1e5129f2c
Chioma Igwe <cigwe072013@gmail.com>
2023-02-21 15:25:10 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - program that prints _putchar
  * followed by a new line
- * Return: always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
