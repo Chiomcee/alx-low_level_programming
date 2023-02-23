14aaf8a7bb779cf66445379c04bc649e1c754258
Chioma Igwe <cigwe072013@gmail.com>
2023-02-22 16:15:21 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - check for positive or negative numbers
+ *
+ * @i: number to be checked
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
