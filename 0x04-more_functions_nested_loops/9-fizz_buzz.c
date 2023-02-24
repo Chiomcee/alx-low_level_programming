2dc8a27954ef7a577f165685ed4cba158bb0073e
Chioma Igwe <cigwe072013@gmail.com>
2023-02-23 19:11:38 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,23 +15,27 @@ int main(void)
 	{
 	if (i % 3 == 0 && i % 5 != 0)
 	{
-	printf(" Fizz ");
+	printf("Fizz");
 	}
 	else if (i % 5 == 0 && i % 3 != 0)
 	{
-	printf(" Buzz ");
+	printf("Buzz");
 	}
 	else if (i % 3 == 0 && i % 5 == 0)
 	{
-	printf(" FizzBuzz ");
+	printf("FizzBuzz");
 	}
 	else if (i == 1)
 	{
-	printf(" %d ", i);
+	printf("%d", i);
 	}
 	else
 	{
-	printf(" %d ", i);
+	printf("%d", i);
+	}
+	if (i != 100)
+	{
+	printf(" ");
 	}
 	}
 	printf("\n");
