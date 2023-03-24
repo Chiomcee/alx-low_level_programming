4e8afc45a858ca8d4751e29affa260b202c48c2e
Chioma Igwe <cigwe072013@gmail.com>
2023-03-23 20:26:32 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,25 +11,21 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
+	va_list args;
 	unsigned int i;
 
-
-	va_start(nums, n);
-
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(nums, int));
+	printf("%d", va_arg(args, int));
 
-
-		if (i != (n - 1) && separator != NULL)
-				printf("%s", separator);
+	if (separator != NULL && i != n - 1)
+	{
+		printf("%s", separator);
+	}
 	}
 
-
+	 va_end(args);
 	printf("\n");
-
-
-	va_end(nums);
 }
