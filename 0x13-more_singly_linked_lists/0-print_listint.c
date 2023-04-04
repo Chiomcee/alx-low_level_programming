1794ac6b336b76718cd294fa6b7d9846d61a0381
Chioma Igwe <cigwe072013@gmail.com>
2023-04-03 22:30:20 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint -    function that prints all element of a list
+ * @h: pointer to the head of the list
+ * Return: number of node in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
