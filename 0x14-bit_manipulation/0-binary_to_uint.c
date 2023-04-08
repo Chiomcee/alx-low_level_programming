c5070a908387559b7c8e532fb444bdf94a7bbf4e
Chioma Igwe <cigwe072013@gmail.com>
2023-04-08 01:16:29 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function that converts binary num to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars
+ *
+ * Return: converted number,or 0 if there is one or
+ *		more chars in the string b that is not 0 or 1, or if b is NULL
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		i = i << 1;
+
+		if (*b++ == '1')
+			i += 1;
+	}
+
+	return (i);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,5 @@
+0x14-bit_manipulation
+
+
+
+
