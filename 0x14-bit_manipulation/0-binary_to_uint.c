aca21e206bd8ca89be376d7a61fd74fa67b57c45
Chioma Igwe <cigwe072013@gmail.com>
2023-04-08 01:41:04 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,19 +11,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
+	unsigned int value = 0;
 
-	if (!b)
+	if (b[i] == '\0')
 		return (0);
 
-	while (*b)
+	while ((b[i] == '0') || (b[i] == '1'))
 	{
-		if (*b != '0' && *b != '1')
-			return (0);
-		i = i << 1;
-
-		if (*b++ == '1')
-			i += 1;
+		value <<= 1;
+		value += b[i] - '0';
+		i++;
 	}
 
-	return (i);
+	return (value);
 }
