3160191f25529f47b739424a216732027fd20677
Chioma Igwe <cigwe072013@gmail.com>
2023-04-10 05:17:19 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,15 +21,19 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
 	if (!text_content)
 		text_content = "";
 
 	for (len = 0; text_content[len]; len++)
+		;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrlen = write(fd, text_content, len);
 
-	if (fd == -1 || wrlen == -1)
+	if (wrlen == -1)
 		return (-1);
 
 	close(fd);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,17 +12,18 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, wrlen, len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (text_content != NULL)
 		return (-1);
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len];len++)
+			;
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
 	wrlen = write(fd, text_content, len);
 
 	if (fd == -1 || wrlen == -1)
