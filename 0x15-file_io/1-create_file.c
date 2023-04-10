16502afdf26038131b326f5a890b3ad7bc4b0382
Chioma Igwe <cigwe072013@gmail.com>
2023-04-10 04:47:57 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * create_file - func that creates a file
@@ -12,15 +18,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wrlen, len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-		return (-1);
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrlen = write(fd, text_content, len);
