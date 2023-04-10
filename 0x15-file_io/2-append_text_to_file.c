63cd5eb44a66d22e07295219a70ecf6d8d19567f
Chioma Igwe <cigwe072013@gmail.com>
2023-04-10 01:01:56 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - func that appends text at the end of a file
+ * @filename: name of file
+ * @text_content: a string to add to the enf of the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wrlen, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		return (-1);
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	wrlen = write(fd, text_content, len);
+
+	if (fd == -1 || wrlen == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
