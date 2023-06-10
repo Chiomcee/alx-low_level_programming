d158eb6e51d060cb7d2b954d78b0a7199a118e13
Chioma Igwe <cigwe072013@gmail.com>
2023-06-10 15:27:28 -0700

    betty corrected

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,8 @@
  * hash_table_create - function that create a hash table
  * @size: size of the array
  *
- * Return: pointer to newly created hash table
+ * Return: if an error occurs - Null
+ *        otherwise pointer to newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
